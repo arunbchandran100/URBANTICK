d0acff231cb564aa6a4628e340ba4ad99dab7ebc
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2024-11-27 16:14:18 +0530

    small fixes

--- controllers/user/myOrdersController.js
+++ controllers/user/myOrdersController.js
@@ -1,38 +1,38 @@
 const Product = require("../../models/productSchema");
 const Category = require("../../models/categoryModel");
 const Variant = require("../../models/variantSchema");
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
 const Cart = require("../../models/cartModel");
 
 
-const Order = require("../../models/orderModel"); 
+const Order = require("../../models/orderModel");
 
 
 exports.getMyOrders = async (req, res) => {
   try {
-    const userId = req.session.user._id; 
-    const page = parseInt(req.query.page) || 1;  
-    const limit = 5;  
-    const skip = (page - 1) * limit;  
+    const userId = req.session.user._id;
+    const page = parseInt(req.query.page) || 1;
+    const limit = 5;
+    const skip = (page - 1) * limit;
 
-     const totalOrders = await Order.countDocuments({ userId });
+    const totalOrders = await Order.countDocuments({ userId });
     const userOrders = await Order.find({ userId })
       .sort({ createdAt: -1 })
       .skip(skip)
       .limit(limit);
 
-     const ordersWithDetails = userOrders.map((order) => ({
-      _id: order._id,  
-      orderDate: order.createdAt.toDateString(),  
-      totalPrice: order.totalPrice,  
-      paymentMethod: order.paymentMethod,  
-      orderStatus: order.orderStatus,  
+    const ordersWithDetails = userOrders.map((order) => ({
+      _id: order._id,
+      orderDate: order.createdAt.toDateString(),
+      totalPrice: order.totalPrice,
+      paymentMethod: order.paymentMethod,
+      orderStatus: order.orderStatus,
       items: order.orderItems.map((item) => ({
-        productName: item.product.productName,  
-        imageUrl: item.product.imageUrl,  
-        color: item.variant.color,  
-        price: item.variant.price, 
-        quantity: item.quantity, 
+        productName: item.product.productName,
+        imageUrl: item.product.imageUrl,
+        color: item.variant.color,
+        discountPrice: item.variant.discountPrice,
+        quantity: item.quantity,
       })),
     }));
 
