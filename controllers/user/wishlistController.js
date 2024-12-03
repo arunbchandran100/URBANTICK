72edeef9792405f2fa616696614704726b7901d9
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2024-12-03 13:24:44 +0530

    User not logged in (Wishlist)

--- controllers/user/wishlistController.js
+++ controllers/user/wishlistController.js
@@ -1,6 +1,6 @@
 const Wishlist = require("../../models/wishlistModel");
 exports.getwishlist = async (req, res) => {
-  res.render("user/wishlist");
+    res.render("user/wishlist");
 };
 
 
@@ -8,41 +8,46 @@ exports.getwishlist = async (req, res) => {
 
 // Add product to wishlist
 exports.addToWishlist = async (req, res) => {
-  try {
-    const { productId, variantId } = req.body;
-    const userId =  req.session.user._id;
+    try {
+        if (!req.session.user) {
+            return res.status(400).json({ error: "User not logged in" });
+        }
 
-    //console.log(userId + " productId " + productId + " variantId " + variantId);
-    if (!productId || !variantId) {
-      return res
-        .status(400)
-        .json({ error: "Product ID and Variant ID are required." });
+        console.log(22222222);
+        const { productId, variantId } = req.body;
+        const userId = req.session.user?._id;
+
+        //console.log(userId + " productId " + productId + " variantId " + variantId);
+        if (!productId || !variantId) {
+            return res
+                .status(400)
+                .json({ error: "Product ID and Variant ID are required." });
+        }
+
+        // Check if the product is already in the wishlist
+        const existingWishlistItem = await Wishlist.findOne({
+            userId,
+            productId,
+            variantId,
+        });
+
+        if (existingWishlistItem) {
+            return res
+                .status(409)
+                .json({ error: "Product is already in your wishlist." });
+        }
+
+        // Add to wconsole.log(8585);
+
+        const newWishlistItem = new Wishlist({ userId, productId, variantId });
+
+        await newWishlistItem.save();
+
+        res.status(201).json({ message: "Product added to wishlist." });
+        // console.log(665522222000000);
+
+    } catch (error) {
+        console.error("Error adding to wishlist:", error);
+        res.status(500).json({ error: "Failed to add to wishlist." });
     }
-
-    // Check if the product is already in the wishlist
-    const existingWishlistItem = await Wishlist.findOne({
-      userId,
-      productId,
-      variantId,
-    });
-
-    if (existingWishlistItem) {
-      return res
-        .status(409)
-        .json({ error: "Product is already in your wishlist." });
-    }
-
-    // Add to wconsole.log(8585);
-
-    const newWishlistItem = new Wishlist({ userId, productId, variantId });
-
-    await newWishlistItem.save();
-
-    res.status(201).json({ message: "Product added to wishlist." });
-    // console.log(665522222000000);
-
-  } catch (error) {
-    console.error("Error adding to wishlist:", error);
-    res.status(500).json({ error: "Failed to add to wishlist." });
-  }
 };
