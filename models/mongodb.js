2b8c106766abcba1201c7105d3cede1afada903c
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2024-12-13 11:48:45 +0530

    Moved mongodb creds to env file

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/URBANTICK")
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("MongoDB is successfully connected");
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
+
