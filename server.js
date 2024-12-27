abcab1ca2e9c92d1591ac79b2445fc5737b13213
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2024-12-27 18:42:46 +0530

    Updated Google sign in URL

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: process.env.callbackURL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
